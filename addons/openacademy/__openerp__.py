4a2bf06fdcf4f3b00eefb06e3ec0bd35051ec97c
ParokshaX <ashok@paroksha.com>
2016-02-23 10:55:54 +0545

    updated openacademy manifest file

--- addons/openacademy/__openerp__.py
+++ addons/openacademy/__openerp__.py
@@ -2,21 +2,22 @@
 {
     'name': "openacademy",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """Manage trainings""",
 
     'description': """
-        Long description of module's purpose
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "ParokshaX",
+    'website': "http://www.paroksha.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Experiment',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
