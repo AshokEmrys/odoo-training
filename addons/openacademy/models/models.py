eec754b5581b15af2c318a5f7bdbfe01da5c47cf
ParokshaX <ashok@paroksha.com>
2016-02-25 12:34:46 +0545

    *Added computed field percen_taken_seats * added progress bar in tree and form view of Session for * percen_taken_seats field

--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -22,6 +22,16 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6,2), help="Duration in days")
     seats = fields.Integer(string="Number of Seats")
+    percen_taken_seats = fields.Float(digits=(2,2), string = "Taken Seats", compute='_percen_taken_seats')
     instructor_id = fields.Many2one('res.partner', string='Instructor', domain=['&', '|', ('instructor', '=', True), ('category_id.name', 'ilike', 'Teacher'), ('company_type', '=', 'person')])
     course_id = fields.Many2one('openacademy.course', string="Course", required=True, ondelete='cascade')
     attendee_ids = fields.Many2many('res.partner', string="Attendeed")
+
+    @api.depends('attendee_ids', 'seats')
+    @api.one
+    def _percen_taken_seats(self):
+        if not self.seats:
+            self.percen_taken_seats = 0.0
+        else:
+            self.percen_taken_seats = 100.0 * len(self.attendee_ids) / self.seats
+
